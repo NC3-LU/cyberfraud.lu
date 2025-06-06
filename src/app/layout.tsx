d20c159b8735b02f9b5ca2675950e1d9a0b498ba
andrealhc <andrea.frittella@nc3.lu>
2025-06-06 11:58:27 +0200

    Fix BCL logo. Fix translations. Add Meta and Snapchat Pixel.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css'
 import i18nConfig from '@/i18nConfig'
 import { notFound } from 'next/navigation'
 import type { Locale } from '@/types'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Cyberfraud.lu',
@@ -25,7 +26,44 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 
   return (
     <html lang={locale} className={`${fjalla.variable} ${roboto.variable}`}>
-      <body>{children}</body>
+      <body>
+        {process.env.NODE_ENV === 'production' ? (
+          <>
+            <Script id={'snapchat-pixel'} strategy={'beforeInteractive'}>
+              {`<!-- Snap Pixel Code -->
+        (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+      {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+        a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+        r.src=n;var u=t.getElementsByTagName(s)[0];
+        u.parentNode.insertBefore(r,u);})(window,document,
+        'https://sc-static.net/scevent.min.js');
+
+        snaptr('init', '535377c6-c8c4-4bcd-a07e-82a720887f13', {
+        'user_email': '__INSERT_USER_EMAIL__'
+      });
+        snaptr('track', 'PAGE_VIEW');
+      <!-- End Snap Pixel Code -->`}
+            </Script>
+            <Script id={'meta-pixel'} strategy={'beforeInteractive'}>
+              {`<!-- Meta Pixel Code -->
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '1229938371838750');
+                  fbq('track', 'PageView');
+                <!-- End Meta Pixel Code -->`}
+            </Script>
+          </>
+        ) : (
+          <></>
+        )}
+        {children}
+      </body>
     </html>
   )
 }
