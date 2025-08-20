7fb7330db4139f7a7292567a319ee6ac37855b8b
Nicky Mezzina <nicky.mezzina@nc3.lu>
2025-08-20 16:04:56 +0200

    Fix typo

--- tests/e2e/cookiebot-provider.spec.ts
+++ tests/e2e/cookiebot-provider.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Cookiebot Live Integration', () => {
 
-  test('should display the real Cookiebot banner on page load and', async ({ page }) => {
+  test('should display the real Cookiebot banner on page load if environment variable NEXT_PUBLIC_CM_COOKIEBOT_CBID is set', async ({ page }) => {
     test.skip(!process.env.NEXT_PUBLIC_CM_COOKIEBOT_CBID, 'Skipping test: Cookiebot CBID environment variable is not set.');
     await page.goto('http://127.0.0.1:3000');
 
