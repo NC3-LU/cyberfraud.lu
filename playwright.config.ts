ab024153420751ca885f7c9d806cfb329c8715f8
Nicky Mezzina <nicky.mezzina@nc3.lu>
2025-08-20 16:31:22 +0200

    Relocate jest and playwright output directories to ./tests/*, also add cleanup scripts for test artifacts.

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests/e2e',
+  /* Path for results and trace files. Defaults to './test-results' */
+  outputDir: './tests/e2e/test-results',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,14 +24,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { outputFolder: './tests/e2e/playwright-report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-first-failure',
   },
 
   /* Configure projects for major browsers */
